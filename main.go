9cea7473ba7018b6051000ceb95b63f0cb3ffbf2
Martin Penckert <martin.penckert@cevt.se>
2022-11-24 08:12:05 +0100

    Adding REPL and main loop

---
+++ main.go
@@ -0,0 +1,21 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	
+	"github.com/felbit/monkey-go/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Printf("Hello %s! This is the Monkey programming language v1.6\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
